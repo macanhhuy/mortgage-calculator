ebc7ab09387ebcf55fff8a02a9b9d27b885d792b
Frank Gutierrez <dearfrankg@gmail.com>
2013-01-29 00:10:17 -0800

    angularjs + ui + ui-bootstrap

---
+++ components/angular-bootstrap/src/collapse/test/collapse.spec.js
@@ -0,0 +1,33 @@
+describe('collapse directive', function () {
+
+  var scope, $compile, $timeout, $transition;
+
+  beforeEach(module('ui.bootstrap.collapse'));
+  beforeEach(inject(function(_$rootScope_, _$compile_, _$timeout_, _$transition_) {
+    scope = _$rootScope_;
+    $compile = _$compile_;
+    $timeout = _$timeout_;
+    $transition = _$transition_;
+  }));
+
+  var element;
+
+  beforeEach(function() {
+    element = $compile('<div collapse="isCollapsed">Some Content</div>')(scope);
+    angular.element(document.body).append(element);
+  });
+
+  it('should collapse if isCollapsed = true', function() {
+    scope.isCollapsed = true;
+    scope.$digest();
+    $timeout.flush();
+    expect(element.height()).toBe(0);
+  });
+
+  it('should expand if isCollapsed = false', function() {
+    scope.isCollapsed = false;
+    scope.$digest();
+    $timeout.flush();
+    expect(element.height()).not.toBe(0);
+  });    
+});
